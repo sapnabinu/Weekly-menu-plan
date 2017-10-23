28b99efae737041976b775bcbeb5eaa1b65a914d
sapnabinu <sapna.g.binu@gmail.com>
2017-10-23 13:54:02 -0700

    changed layout

--- plan.js
+++ plan.js
@@ -1,4 +1,76 @@
 var eventdiv = null;
+var spinWidget = null;
+var curRows = 0;
+
+Date.prototype.addDays = function(days) {
+  var dat = new Date(this.valueOf());
+  dat.setDate(dat.getDate() + days);
+  return dat;
+}
+
+function makeCell(mealType, row) {
+    var div = document.createElement("div");
+    div.className = "demo-content";
+    div.onclick = "showChoice(event)";
+    div.id = mealType + ":" + row;
+    div.innerHTML = "--choose--";
+
+    var divWrap = document.createElement("div");
+    divWrap.className = "col-md-3";
+    divWrap.appendChild(div);
+    return divWrap;
+}
+
+function makeDateCell(row) {
+    var div = document.createElement("div");
+    div.className = "demo-content";
+    div.id = "DATE" + ":" + row;
+
+    var rowDate = $("#datepicker").datepicker("getDate").addDays(row);
+    div.innerHTML = $.datepicker.formatDate("yy-mm-dd", rowDate)
+
+    var divWrap = document.createElement("div");
+    divWrap.className = "col-md-3";
+    divWrap.appendChild(div);
+    return divWrap;
+}
+
+function makeRow(rowIndex) {
+    var row = document.createElement("div");
+    row.className = "row";
+    row.id = "ROW:" + rowIndex;
+
+    var date = makeDateCell(rowIndex);
+    var bfst = makeCell("BFST", rowIndex);
+    var lnch = makeCell("LNCH", rowIndex);
+    var dnnr = makeCell("DNNR", rowIndex);
+
+    row.appendChild(date);
+    row.appendChild(bfst);
+    row.appendChild(lnch);
+    row.appendChild(dnnr);
+    return row;
+}
+
+function generateRows() {
+    // First check whether to add, delete or do nothing
+    var reqRows = $("#spinner").spinner("value")
+    if (curRows == reqRows) {
+	// do nothing
+    } else if (curRows < reqRows) {
+	// add rows
+	for (var row=curRows; row < reqRows; row++) {
+	    document.getElementById("plancontainer").appendChild(makeRow(row+1));
+	}
+    } else {
+	// delete rows
+	for(var row=curRows; row>reqRows; row--) {
+	    var delDiv = document.getElementById("ROW:" + row);
+	    document.getElementById("plancontainer").removeChild(delDiv);
+	}
+    }
+    curRows = reqRows;
+}
 
 function showChoice(event) {
     eventdiv = event.target
@@ -13,4 +85,18 @@ function showChoice(event) {
 function chooseRecipe(event) {
     eventdiv.innerHTML = event.target.dataset.rname;
     $("#recipedialog").dialog("close");
+}
+
+function saveMenu(event) {
+    var postData = [];
+    $("div[onclick*='showChoice']").each(function() {
+	var typeRow = this.id.split(":");
+	var mealType = typeRow[0];
+	var mealRow = typeRow[1];
+	var mealDate = $("#datepicker").datepicker("getDate");
+	postData.push(["", mealType, mealDate, this.innerHTML])
+    })
+    
+    var p = $.post("addplan", JSON.stringify(postData));
+    p.done(function(data) { $("#planStatus").innerHTML = "saved"; });
 }
--- server.py
+++ server.py
@@ -1,13 +1,84 @@
 #!/usr/bin/env python
 
-import SimpleHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+import pprint
 
-class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def end_headers(self):
+class S(BaseHTTPRequestHandler):
+
+    mimeMap = { "html" : "text/html",
+                "js" : "application/javascript",
+                "css" : "text/css",
+                "jpg" : "image/jpeg" }
+    
+    def _set_headers(self, extension):
+        mimetype = self.mimeMap.get(extension)
+        if not mimetype:
+            self.send_response(404)
+        else:
+            self.send_response(200)
+        self.send_header('Content-type', mimetype)
         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
         self.send_header("Pragma", "no-cache")
         self.send_header("Expires", "0")
-        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+        self.end_headers()
 
-if __name__ == '__main__':
-    SimpleHTTPServer.test(HandlerClass=MyHTTPRequestHandler)
+        return bool(mimetype)
+
+    def do_GET(self):
+        try:
+            ext = self.path.rsplit(".")[-1]
+            print self.path, ext, self.mimeMap.get(ext, "text/html")
+            f = open(self.path[1:], "rb")
+            if self._set_headers(ext):
+                self.wfile.write(f.read())
+        except:
+            self._set_headers(None)
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def addPlan(self, data):
+        for d in data:
+            pprint.pprint(d)
+        
+    def do_POST(self):
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+        data = json.loads(self.data_string)
+
+        if self.path == "/addplan":
+            self.addPlan(data)
+        else:
+            print "Nothing to do"
+
+        self._set_headers("html")
+        return
+
+def run(server_class=HTTPServer, handler_class=S, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+if len(argv) == 2:
+    run(port=int(argv[1]))
+else:
+    run()
+
+# 
+# import SimpleHTTPServer
+# 
+# class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+#     def end_headers(self):
+#         self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+#         self.send_header("Pragma", "no-cache")
+#         self.send_header("Expires", "0")
+#         SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+# 
+# if __name__ == '__main__':
+#     SimpleHTTPServer.test(HandlerClass=MyHTTPRequestHandler)
+# 
