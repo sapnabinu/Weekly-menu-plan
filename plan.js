56f3e376eec44e7ec37b8607a8adcd8425166bac
sapnabinu <sapna.g.binu@gmail.com>
2017-12-06 12:20:12 -0800

    ingredients change

--- plan.js
+++ plan.js
@@ -2,7 +2,7 @@ var eventdiv = null;
 var spinWidget = null;
 var curRows = 0;
 var recipeList = null;
-var planList = {};
+planList = {};
 var authCookie = null;
 var userName = null;
 var recipeNameTimeMap = {}
@@ -15,6 +15,7 @@ Date.prototype.addDays = function(days) {
 
 function menuplanSetup() {
     document.getElementById("menuinfo").onchange=startDateChange;
+    document.getElementById("typeofmeal").onchange=startDateChange;
     $("#datepicker").datepicker({ onSelect: startDateChange } );
     $("#datepicker").datepicker("setDate", "0");
     
@@ -24,12 +25,12 @@ function menuplanSetup() {
                                          });
     function assignRecipeList () {
         recipeList = JSON.parse(this.responseText);
-	var entry;
-	Object.keys(recipeList).forEach(function(lstName, ri) {
-	    recipeList[lstName].forEach(function(recipe, i) {
-		recipeNameTimeMap[recipe[0] + ":" + recipe[1]] = recipe;
-	    });
-	});
+        var entry;
+        Object.keys(recipeList).forEach(function(lstName, ri) {
+            recipeList[lstName].forEach(function(recipe, i) {
+                recipeNameTimeMap[recipe[0] + ":" + recipe[1]] = recipe;
+            });
+        });
     }
 
     var xhr = new XMLHttpRequest();
@@ -39,22 +40,50 @@ function menuplanSetup() {
     generateRows();
 };
 
-function makeCell(mealType, row, mealDate) {
+
+function chooseMeal(mealType, typeofmeal) {
+    var meals = recipeList[mealType];
+    var recipe = null;
+    var typeIndex = typeofmeal == "Low calorie" ? 9 : typeofmeal == "Kids meal" ? 7 : 8;
+
+    var count = 60;
+    while (count-- > 0) {
+        for(var i=0; i<meals.length; i++) {
+            recipe = meals[i];
+            var rand = Math.round(Math.random() * 10);
+            if (recipe[typeIndex] == 1 && rand == 7) {
+                console.log("Returning", recipe, "for", mealType, typeofmeal);
+                return recipe;
+            }
+        }
+    }
+    alert("No suitable recipe found for " + typeofmeal);
+    return null;
+}
+
+function makeCell(mealType, row, mealDate, typeofmeal) {
     var div = document.createElement("div");
     div.className = "demo-content";
     div.onclick = showChoice;
     div.id = "MEALCELL:" + mealType + ":" + row;
 
     var mealTag = mealType + ":" + mealDate;
-    var planChoice = planList[mealTag];
-    if (planChoice == null) {
-        div.innerHTML = "--choose--";
+    var recipe;
+
+    if (typeofmeal != "Type of Meal") {
+        recipe = chooseMeal(mealType, typeofmeal);
+        planList[mealType + ":" + mealDate] = ["", mealType, mealDate, recipe[0]];
+        div.style.backgroundColor = "salmon";
     } else {
-	var e = document.getElementById("menuinfo");
-	var idx = e.options[e.selectedIndex].value;
-	var recipe = recipeNameTimeMap[planChoice[3] + ":" + planChoice[1]];
-        div.innerHTML = makeMealHtml(recipe);
+        var planChoice = planList[mealTag];
+        if (planChoice == null) {
+            recipe = null;
+        } else {
+            recipe = recipeNameTimeMap[planChoice[3] + ":" + planChoice[1]];
+        }
     }
+    
+    div.innerHTML = makeMealHtml(recipe);
 
     var divWrap = document.createElement("div");
     divWrap.className = "col-md-3";
@@ -74,7 +103,7 @@ function makeDateCell(row, rowDate) {
     return divWrap;
 }
 
-function makeRow(rowIndex) {
+function makeRow(rowIndex, typeofmeal) {
     var rowDiv = document.createElement("div");
     rowDiv.className = "row";
     rowDiv.id = "ROW:" + rowIndex;
@@ -83,9 +112,9 @@ function makeRow(rowIndex) {
     rowDate = $.datepicker.formatDate("yy-mm-dd", rowDate);
 
     var date = makeDateCell(rowIndex, rowDate);
-    var bfst = makeCell("BFST", rowIndex, rowDate);
-    var lnch = makeCell("LNCH", rowIndex, rowDate);
-    var dnnr = makeCell("DNNR", rowIndex, rowDate);
+    var bfst = makeCell("BFST", rowIndex, rowDate, typeofmeal);
+    var lnch = makeCell("LNCH", rowIndex, rowDate, typeofmeal);
+    var dnnr = makeCell("DNNR", rowIndex, rowDate, typeofmeal);
 
     rowDiv.appendChild(date);
     rowDiv.appendChild(bfst);
@@ -99,11 +128,11 @@ function generateRows() {
         var plan = JSON.parse(this.responseText);
         planList = {}
         for (i=0; i < plan.length; i++) {
-	    var mealType = plan[i][1];
-	    var mealDate = plan[i][2];
+            var mealType = plan[i][1];
+            var mealDate = plan[i][2];
             planList[mealType + ":" + mealDate] = plan[i];
         }
-	onPlanFetch();
+        onPlanFetch();
     }
 
     var dateObj = $("#datepicker").datepicker("getDate");
@@ -117,6 +146,9 @@ function generateRows() {
 }
 
 function onPlanFetch() {
+    var e = document.getElementById("typeofmeal");
+    var typeofmeal = e.options[e.selectedIndex].text;
+
     // First check whether to add, delete or do nothing
     var reqRows = $("#spinner").spinner("value")
     if (curRows == reqRows) {
@@ -124,7 +156,7 @@ function onPlanFetch() {
     } else if (curRows < reqRows) {
         // add rows
         for (var row=curRows; row < reqRows; row++) {
-            document.getElementById("plancontainer").appendChild(makeRow(row+1));
+            document.getElementById("plancontainer").appendChild(makeRow(row+1, typeofmeal));
         }
     } else {
         // delete rows
@@ -148,16 +180,25 @@ function startDateChange() {
 function makeRecipeDiv(mealTime, row) {
     var recipe = recipeList[mealTime][row];
     var div = document.createElement("div");
-    div.onclick = chooseRecipe;
-    div.dataset.recipe = JSON.stringify([mealTime, row]);
-    div.innerHTML = recipe[0];
+	
+	var textDiv = document.createElement("div");
+    textDiv.onclick = chooseRecipe;
+    textDiv.dataset.recipe = JSON.stringify([mealTime, row]);
+    textDiv.innerHTML = recipe[0];
+	textDiv.style.cssFloat = "left";
 
-    var imgDiv = document.createElement("img");
-    imgDiv.src = recipe[10];
-    imgDiv.height = 50;
-    imgDiv.width = 50;
+    var img = document.createElement("img");
+    img.src = recipe[10];
+    img.height = 50;
+    img.width = 50;
+	img.style.cssFloat = "right";
 
-    div.appendChild(imgDiv);
+	var clearDiv = document.createElement("div");
+	clearDiv.style.clear = "both";
+	
+	div.appendChild(textDiv);
+    div.appendChild(img);
+	div.appendChild(clearDiv);
     return div;
 }
 
@@ -203,6 +244,10 @@ function chooseRecipe(event) {
 }
 
 function makeMealHtml(recipe) {
+    if (recipe === null) {
+        return "--choose--";
+    }
+
     var e = document.getElementById("menuinfo");
     var idx = e.options[e.selectedIndex].value;
     var idxType = e.options[e.selectedIndex].text;
@@ -215,14 +260,19 @@ function makeMealHtml(recipe) {
 function saveMenu(event) {
     var postData = [];
     $("div[id*='MEALCELL']").each(function() {
-	if (this.style.backgroundColor == "salmon") {
+        if (this.style.backgroundColor == "salmon") {
             var mealTime = this.id.split(":")[1];
             var mealRow = this.id.split(":")[2];
             var mealDate = document.getElementById("DATE:"+mealRow).innerHTML;
             postData.push(planList[mealTime + ":" + mealDate]);
-	    this.style.backgroundColor = "#dbdfe5";
+            this.style.backgroundColor = "#dbdfe5";
         }
     })
+    
+    if (postData.length === 0) {
+        alert("No recipes in plan, nothing to do");
+        return;
+    }
     
     var p = $.post("addplan", JSON.stringify(postData));
     p.done(function(data) { $("#planStatus").innerHTML = "saved"; });
@@ -282,17 +332,17 @@ function login(event) {
     var xhr = new XMLHttpRequest();
 
     function loginResponse() {
-	if (xhr.status == 200) {
+        if (xhr.status == 200) {
             authcookie = JSON.parse(this.responseText)["authcookie"];
-	    document.getElementById("useremail").style.display = "none";
-	    document.getElementById("password").style.display = "none";
-	    document.getElementById("loginbutton").style.display = "none";
-	    document.getElementById("logoutbutton").style.display = "block";
+            document.getElementById("useremail").style.display = "none";
+            document.getElementById("password").style.display = "none";
+            document.getElementById("loginbutton").style.display = "none";
+            document.getElementById("logoutbutton").style.display = "block";
 
-	    document.cookie = "authcookie=" + authcookie +";path=/";
-	} else {
-	    alert("Login for " + userName + " failed" );
-	}
+            document.cookie = "authcookie=" + authcookie +";path=/";
+        } else {
+            alert("Login for " + userName + " failed" );
+        }
     }
 
     xhr.open("POST", '/login', true);
@@ -334,18 +384,18 @@ function newuser(event) {
 
 function logout(event) {
     function logoutResponse() {
-	if (xhr.status == 200) {
-	    document.getElementById("useremail").style.display = "block";
-	    document.getElementById("password").style.display = "block";
-	    document.getElementById("loginbutton").style.display = "block";
-	    document.getElementById("logoutbutton").style.display = "none";	    
+        if (xhr.status == 200) {
+            document.getElementById("useremail").style.display = "block";
+            document.getElementById("password").style.display = "block";
+            document.getElementById("loginbutton").style.display = "block";
+            document.getElementById("logoutbutton").style.display = "none";         
 
-	    userName = null;
-	    authcookie = null;
-	    document.cookie = "authcookie=;path=/;";
-	} else {
-	    alert("Logout for " + userName + " failed" );
-	}
+            userName = null;
+            authcookie = null;
+            document.cookie = "authcookie=;path=/;";
+        } else {
+            alert("Logout for " + userName + " failed" );
+        }
     }
 
     var xhr = new XMLHttpRequest();
@@ -354,15 +404,148 @@ function logout(event) {
     xhr.send(JSON.stringify({}));
 }
 
+function addCell(trElement, text, element) {
+    var el = document.createElement(element);
+    el.appendChild(document.createTextNode(text))
+    trElement.appendChild(el);
+}    
+
+// Summary table required?
+// { ingredient name => [ ingredient count, ingredient unit, recipe count ], ... }
+// Ingredient Name, number of meals, number of people, total quantity, unit of measurement
+function buildIngredientTable(ingredientList) {
+    var page = ""
+    var tbl = document.createElement("table");
+    var peoplecount = document.getElementById("peoplecount").value;
+    tbl.className = "ingredients";
+
+    var tr = document.createElement('tr');
+    addCell(tr, "Ingredient Name", "th")
+    addCell(tr, "Number of meals", "th")
+    addCell(tr, "Number of people", "th")
+    addCell(tr, "Total quantity", "th")
+    addCell(tr, "Unit", "th")
+    tbl.appendChild(tr);
+
+    page += sprintf("<span style='font: 400 18px courier;'>%-24s, %6s, %8s</h2><br/><hr/>",
+                    "Ingredient Name", "Quantity", "Unit");
+    var entry;
+    var tr;
+    Object.keys(ingredientList).forEach(function(ingredient, _index) {
+        entry = ingredientList[ingredient];
+        tr = document.createElement('tr');
+
+        addCell(tr, ingredient, "td")
+        addCell(tr, entry[2], "td")
+        addCell(tr, peoplecount, "td")
+        addCell(tr, peoplecount * entry[2], "td")
+        addCell(tr, entry[1], "td")
+
+        tbl.appendChild(tr);
+
+        page += sprintf("<span style='font: 400 16px courier;'>%-24s, %6s, %8s</span><br>",
+                        ingredient, peoplecount * entry[2], entry[1]);
+    });
+
+    return [tbl, page];
+}
+
+
+function showingredients(event) {
+    // For each recipe count occurrence
+    // For all each recipe get ingredients => [ recipe, ingredient, quantity, measure ]
+    // Build ingredient -> count map
+    recipeCountMap = {}
+    var mealTime, mealRow, mealDate, recipe;
+    $("div[id*='MEALCELL']").each(function() {
+        if (this.innerHTML != "--choose--") {
+            mealTime = this.id.split(":")[1];
+            mealRow = this.id.split(":")[2];
+            mealDate = document.getElementById("DATE:"+mealRow).innerHTML;
+            recipe = planList[mealTime + ":" + mealDate][3];
+
+            if (!(recipe in recipeCountMap)) {
+                recipeCountMap[recipe] = 0;
+            }
+            recipeCountMap[recipe] += 1;
+        }
+    })
+
+    if (Object.keys(recipeCountMap).length === 0) {
+        alert("No recipes in plan, nothing to do");
+        return;
+    }
+    
+    function gotIngredients() {
+        if (xhr.status == 200) {
+            var recipeIngList = JSON.parse(this.responseText);
+            var ingredientCountMap = {};
+
+            // Each row is: recipe name, ingredient, quantity, measure
+            recipeIngList.forEach(function(ingredientEntry, i) {
+                recipeName = ingredientEntry[0];
+                ingredientName = ingredientEntry[1];
+                ingredientQuantity = ingredientEntry[2];
+
+                if ( ! (ingredientName in ingredientCountMap)) {
+                    // ingredient       =>               [ count, measure, recipe-count ]
+                    ingredientCountMap[ingredientName] = [ 0, ingredientEntry[3], 0];
+                }
+                entry = ingredientCountMap[ingredientName];
+                entry[0] += ingredientQuantity;
+                entry[2] += recipeCountMap[recipeName];
+            });
+
+            var dlgDiv = document.getElementById("inglistdialog");
+            while (dlgDiv.firstChild) {
+                dlgDiv.removeChild(dlgDiv.firstChild);
+            }
+
+            var rval = buildIngredientTable(ingredientCountMap);
+            var ingredientTable = rval[0];
+            var printText = rval[1];
+            var showIngDiv = document.createElement("div")
+            showIngDiv.id = "ingredientTableDiv"
+
+            if (event.id = "printingeredients") {
+                var printDiv = document.createElement("div")
+                printDiv.onclick = function() {
+                    var originalContents = document.body.innerHTML;
+                    document.body.innerHTML = printText;
+                    window.print();
+                    document.body.innerHTML = originalContents;
+                    $("#inglistdialog").dialog('close');
+                };
+                printDiv.className = "div_hover";
+                printDiv.height = 20;
+                printDiv.appendChild(document.createTextNode("Print"));
+                showIngDiv.appendChild(printDiv);
+            }
+
+            showIngDiv.appendChild(ingredientTable);
+            dlgDiv.appendChild(showIngDiv);
+            $("#inglistdialog").dialog({ title: 'Ingredients', height: 500, width: 600});
+            $("#inglistdialog").dialog('open');
+        } else {
+            alert("Internal error: failed to fetch ingredient details from server" );
+        }
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", gotIngredients);
+    xhr.open("POST", "/getingredients");
+    xhr.send(JSON.stringify(Object.keys(recipeCountMap)));
+}
+
 function kidsMealClick(event) {
     var kidsmealdiv = document.getElementById("plancontainer");
 
     function kidsMealResponse() {
-	if (xhr.status == 200) {
-	    kidsmealdiv.innerHTML = this.responseText;
-	} else {
-	    alert("/getkidsmeal failed");
-	}
+        if (xhr.status == 200) {
+            kidsmealdiv.innerHTML = this.responseText;
+        } else {
+            alert("/getkidsmeal failed");
+        }
     }
 
     var xhr = new XMLHttpRequest();
